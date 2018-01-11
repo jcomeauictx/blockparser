da16c66301ea9073a5cca7e90d8e54cee1674697
John Comeau <jc@unternet.net>
2018-01-10 20:12:07 -0800

    started on script

--- parser.py
+++ parser.py
@@ -27,6 +27,15 @@ VARINT = {
     0xfe: ('<L', 1, 4),
     0xff: ('<Q', 1, 8),
 }
+SCRIPT_OPS = tuple(
+    (n, ('(PUSH(%d))' % n,
+    'stack.push(bytes(script[:%d])); script[:%d]=[]' % (n, n)))
+    for n in range(0x01, 0x4c))
+SCRIPT_OPS += (
+    (0x00, ('OP_FALSE', 'stack.push(0)')),
+    (0x4c, ('OP_PUSHDATA1', 'stack.push(program.eat(1))')),
+    (0x76, ('OP_DUP', 'stack.push(stack[-1])')),
+)
 
 def parse(blockfile=DEFAULT_BLOCK, minblock=0, maxblock=sys.maxsize):
     '''

4008fc9749632f2fc563bcd88f7397a53216e9e4
John Comeau <jc@unternet.net>
2018-01-14 12:12:02 -0800

    it works so far

--- script.py
+++ script.py
@@ -35,32 +35,38 @@ SCRIPT_OPS += (
         'stack.pop(-1); stack[-1] = 1'],  # FIXME: simulating success for now
     ),
 )
-TESTSCRIPT = (  # from Satoshi's genesis block coinbase output
-        b"A\x04g\x8a\xfd\xb0\xfeUH'\x19g\xf1\xa6q0\xb7\x10\\\xd6\xa8"
-        b"(\xe09\t\xa6yb\xe0\xea\x1fa\xde\xb6I\xf6\xbc?L\xef8\xc4\xf3"
-        b"U\x04\xe5\x1e\xc1\x12\xde\\8M\xf7\xba\x0b\x8dW\x8aLp+k\xf1\x1d_\xac"
+TESTSCRIPTS = (  # from Satoshi's genesis block
+    b'\x04\xff\xff\x00\x1d'
+    b'\x01\x04EThe Times 03/Jan/2009 Chancellor on'
+    b' brink of second bailout for banks',
+
+    b"A\x04g\x8a\xfd\xb0\xfeUH'\x19g\xf1\xa6q0\xb7\x10\\\xd6\xa8"
+    b"(\xe09\t\xa6yb\xe0\xea\x1fa\xde\xb6I\xf6\xbc?L\xef8\xc4\xf3"
+    b"U\x04\xe5\x1e\xc1\x12\xde\\8M\xf7\xba\x0b\x8dW\x8aLp+k\xf1\x1d_\xac"
 )
 
-def display(scriptbinary):
+def display(scripts):
     '''
     breaks down binary script into something readable (to a FORTHer)
     '''
     stack = []
     opcodes = dict(SCRIPT_OPS)
-    script = list(scriptbinary)  # gives list of numbers (`ord`s)
-    while script:
-        opcode = script.pop(0)
-        operation = opcodes.get(opcode, None)
-        if operation is None:
-            stack.append(hex(opcode) + "(not yet implemented)")
-        else:
-            display_op = operation[0]
-            logging.debug('`exec`ing operation 0x%x, %s', opcode, display_op)
-            exec(display_op, {**globals(), **locals()})
-    while stack:
-        print(stack.pop(0))
+    for scriptbinary in scripts:
+        script = list(scriptbinary)  # gives list of numbers (`ord`s)
+        while script:
+            opcode = script.pop(0)
+            operation = opcodes.get(opcode, None)
+            if operation is None:
+                stack.append(hex(opcode) + "(not yet implemented)")
+            else:
+                display_op = operation[0]
+                logging.debug('`exec`ing 0x%x, %s', opcode, display_op)
+                exec(display_op, {**globals(), **locals()})
+        while stack:
+            print(stack.pop(0))
+        print('-----')
 
-def run(scriptbinary):
+def run(scripts):
     '''
     executes script the same way (hopefully) as Bitcoin Core would
 
@@ -69,21 +75,22 @@ def run(scriptbinary):
     stack = []
     opstack = []
     opcodes = dict(SCRIPT_OPS)
-    script = list(scriptbinary)  # gives list of numbers (`ord`s)
-    while script:
-        opcode = script.pop(0)
-        operation = opcodes.get(opcode, None)
-        if operation is None:
-            raise NotImplementedError('unrecognized opcode 0x%x' % opcode)
-        else:
-            run_op = operation[1]
-            logging.info('`exec`ing operation 0x%x, %s', opcode, run_op)
-            exec(run_op, {**globals(), **locals()})
-        logging.info('stack: %s', stack)
+    for scriptbinary in scripts:
+        script = list(scriptbinary)  # gives list of numbers (`ord`s)
+        while script:
+            opcode = script.pop(0)
+            operation = opcodes.get(opcode, None)
+            if operation is None:
+                raise NotImplementedError('unrecognized opcode 0x%x' % opcode)
+            else:
+                run_op = operation[1]
+                logging.info('`exec`ing operation 0x%x, %s', opcode, run_op)
+                exec(run_op, {**globals(), **locals()})
+            logging.info('stack: %s', stack)
 
 if __name__ == '__main__':
-    SCRIPT = a2b_hex(sys.argv[1]) if len(sys.argv) > 1 else TESTSCRIPT
-    logging.debug('Displaying script %s...', b2a_hex(SCRIPT))
-    display(SCRIPT)
-    logging.debug('Running script...')
-    run(SCRIPT)
+    SCRIPTS = list(map(a2b_hex, sys.argv[1:])) if sys.argv[1:] else TESTSCRIPTS
+    logging.debug('Displaying scripts %s...', sys.argv[1:] or TESTSCRIPTS)
+    display(SCRIPTS)
+    logging.debug('Running scripts...')
+    run(SCRIPTS)
